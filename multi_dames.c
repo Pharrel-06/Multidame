1322e290a88b0d6be64685b5361bd021288b306c
Pharrel-06 <pharrel.fleuranvil@edu.univ-eiffel.fr>
2025-10-29 11:26:05 +0100

    finision fct obligatoire + correction fct affichage

--- multi_dames.c
+++ multi_dames.c
@@ -32,32 +32,105 @@ typedef struct {
 // Le joueur courant capture la pièce se trouvant à la ligne i et la colonne j
 int jeu_capturer(Jeu *jeu, int i, int j) {
 
+    // On vérifie si la case sélectionné est valide
+    if (!(0 <= i && i < TAILLE) || !(0 <= j && j < TAILLE) || jeu->plateau.pion[i][j] == 0) {
+        return 0;
+    }
+
+    // Ajout du score de la capture
+    switch (jeu->plateau.pion[i][j])
+    {
+    // Pion blanc qui vaut 1
+    case 1:
+        jeu->joueur[jeu->joueur_courant].score += 1;
+        break;
+    // Pion rouge qui vaut 5
+    case 2:
+        jeu->joueur[jeu->joueur_courant].score += 5;
+        break;
+    // Pion noir qui vaut 8
+    case 3:
+        jeu->joueur[jeu->joueur_courant].score += 8;
+        break;
+    }
+
     // Suppression du pion dans le plateau de jeu
     jeu->plateau.pion[i][j] = 0;
-    // Augmentation du score du joueur en question
-    jeu->joueur[jeu->joueur_courant].score ++;
 
-    return 0;
+    // La capture c'est bien passé
+    return 1;
+}
+
+// Vérifie si le pion sélectionné peut faire au moins 1 saut et renvoie les coordonnées possible
+int pos_saut_possible(Jeu *jeu, int pion_i, int pion_j, int indice_saut[][8]) {
+
+    int saut_possible = 0;
+
+    // Parcours des cases où l'on peut sauté
+    for(int i = -2; i <= 2; i+=2) {
+        for(int j = -2; j <= 2; j+=2) {
+            if (0 <= pion_i + i && pion_i + i <= 7 && 0 <= pion_j + j && pion_j + j <= 7) {
+                // Si la case adjacente contient un pion et que la suivante est vide
+                if (jeu->plateau.pion[pion_i + i][pion_j + j] == 0 && jeu->plateau.pion[pion_i + (i/2)][pion_j + (j/2)] != 0) {
+                    // On save les coordonnées de saut dans la liste
+                    indice_saut[0][saut_possible] = pion_i + i;
+                    indice_saut[1][saut_possible] = pion_j + j;
+                    saut_possible ++;
+                }
+            }  
+        }
+    }
+    // Renvoie du nombre de saut possible
+    return saut_possible;
 }
 
 // Utilisée en début de tour pour saisir le pion permettant de faire des saut (par le joueur courant)
 int jeu_saisir_pion(Jeu *jeu, int i, int j)  {
+
+    // Vérifie si la case contient un pion
+    if (jeu->plateau.pion[i][j] == 0) {
+        return 0;
+    }
+
+    // Vérifie si le pion peut faire au moins 1 saut
+    int indice_saut[2][8];
+    if (pos_saut_possible(indice_saut, i, j, jeu) == 0) {
+        return 0;
+    }
 
     // On indique dans la structure "jeu" qu'un pion a été saisie
     jeu->pion_est_saisi = 1;
     jeu->pion_i = i;
     jeu->pion_j = j;
 
-    return 0;
+    // La sélection est correcte et c'est bien passé
+    return 1;
 }
 
 // Fait sauter le pion saisi vers la case (vide) en position (i,j) en capturant le pion entre les deux positions au passage
 int jeu_sauter_vers(Jeu *jeu, int i, int j) {
-    
+
+    // On vérifie si la case sélectionné pour le saut est bien vide
+    if (jeu->plateau.pion[i][j] != 0) {
+        return 0;
+    }
+    // On vérifie si la case sélectionné pour le saut est bien une case adjacente
+    if (!(-2 <= (i - jeu->pion_i) && (i - jeu->pion_i) <= 2 && -2 <= (j - jeu->pion_j) && (j - jeu->pion_j) <= 2)) {
+        return 0;
+    }
+    if (((i - jeu->pion_i) + (j - jeu->pion_j)) % 2 != 0) {
+        return 0;
+    }
+
     // Repérage du pion capturé
     int pion_capture_i, pion_capture_j;
-    pion_capture_i = (i - jeu->pion_i) / 2;
-    pion_capture_j = (j - jeu->pion_j) / 2;
+    pion_capture_i = jeu->pion_i + ((i - jeu->pion_i) / 2);
+    pion_capture_j = jeu->pion_j + ((j - jeu->pion_j) / 2);
+
+    // On vérifie que le saut s'effectue bien sur une case qui contient un pion
+    if (jeu->plateau.pion[pion_capture_i][pion_capture_j] == 0) {
+        return 0;
+    }
 
     // Ajout du score de la capture
     switch (jeu->plateau.pion[pion_capture_i][pion_capture_j])
@@ -87,12 +160,23 @@ int jeu_sauter_vers(Jeu *jeu, int i, int j) {
     jeu->pion_i = i;
     jeu->pion_j = j;
 
-    return 0;
+    return 1;
 }
 
 // Utilisée en début de tour pour arrêter de jouer (tant que le joueur courant n’est pas le dernier joueur)
 int jeu_arreter(Jeu *jeu) {
 
+    // On parcours la liste des joueurs de la partie
+    for(int i = 1; i < jeu->nb_joueurs; i++) {
+        // On vérifie que le joueur suivant continue de jouer grâce à son état
+        if (jeu->joueur[(jeu->joueur_courant + i) % jeu->nb_joueurs].etat == 1) {
+            // Le joueur actuel s'arrête
+            jeu->joueur[jeu->joueur_courant].etat = 0;
+            // Le joueur actuel peut s'arrêté
+            return 1;
+        }
+    }
+    // Le joueur actuel ne peut pas s'arrêté
     return 0;
 }
 
@@ -120,7 +204,7 @@ int jeu_joueur_suivant(Jeu *jeu) {
     return 0;
 }
 
-// Remplie la strcture jeu en fonction des normes demandés dans l'énoncé
+// Remplie la structure jeu en fonction des normes demandés dans l'énoncé
 void jeu_charger(Jeu *jeu) {
 
     // Lecture nombre de joureur, tour actuel et joueur courant
@@ -134,7 +218,7 @@ void jeu_charger(Jeu *jeu) {
     // Lecture pion_est_saisie, i, j
     scanf(" %d %d %d", &(jeu->pion_est_saisi), &(jeu->pion_i), &(jeu->pion_j));
 
-    // Lecture des élément du plateau
+    // Lecture des éléments du plateau
     for(int i = 0; i < TAILLE; i++) {
         for(int j = 0; j < TAILLE; j++) {
             scanf(" %d",&(jeu->plateau.pion[i][j]));
@@ -169,33 +253,33 @@ void jeu_ecrire(Jeu *jeu) {
 // Afffichage interface
 
 void affiche_score(Jeu *jeu){
-    switch (jeu.nb_joueurs){
+    switch (jeu->nb_joueurs){
         case 2: // 2 joueurs
             printf("Score:\n    J1  J2\n    %d  %d\n",
-                jeu.joueur[0].score,
-                jeu.joueur[1].score);
+                jeu->joueur[0].score,
+                jeu->joueur[1].score);
             break;
         case 3: // 3 joueurs
             printf("Score:\n    J1  J2  J3\n    %d  %d  %d\n",
-                jeu.joueur[0].score,
-                jeu.joueur[1].score,
-                jeu.joueur[2].score);
+                jeu->joueur[0].score,
+                jeu->joueur[1].score,
+                jeu->joueur[2].score);
             break;
         default:
             printf("Score:\n    J1  J2  J3  J4\n    %d  %d  %d  %d\n",
-                jeu.joueur[0].score,
-                jeu.joueur[1].score,
-                jeu.joueur[2].score,
-                jeu.joueur[3].score);
+                jeu->joueur[0].score,
+                jeu->joueur[1].score,
+                jeu->joueur[2].score,
+                jeu->joueur[3].score);
     }
 }
 
 void affiche_tour(Jeu *jeu){
-    printf("Tour: %d\n", jeu.tour);
+    printf("Tour: %d\n", jeu->tour);
 }
 
 void affiche_joueur_courant(Jeu *jeu){
-    printf("Joueur %d (%d)\n",jeu.joueur_courant ,jeu.joueur[joueur_courant].score);
+    printf("Joueur %d (%d)\n",jeu->joueur_courant ,jeu->joueur[jeu->joueur_courant].score);
 }
 
 // Choix unique au premier tour
@@ -230,7 +314,7 @@ void affiche_plateau(Jeu *jeu){
     for(int li = 0; li < TAILLE; li++){
         printf("%d  |", li+1);
         for(int col = 0; col < TAILLE; col++){
-            switch(jeu.plateau.pion[li][col]){
+            switch(jeu->plateau.pion[li][col]){
                 case 1: printf("+ "); break;
                 case 2: printf("o "); break;
                 case 3: printf("x "); break;
